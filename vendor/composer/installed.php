fd24d2926cc410ea2c19ec896701b36b2076bd84
egomezg-udc <egomezg@unicartagena.edu.co>
2026-04-14 19:57:48 -0500

    feat(views): página de recuperación de clave e integración con log de correos

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'u/ingenieria-software-carreras-taxi',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8898a2bab494b397f80c1b3fd5b714d189077ff5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'u/ingenieria-software-carreras-taxi' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8898a2bab494b397f80c1b3fd5b714d189077ff5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
